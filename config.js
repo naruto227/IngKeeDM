9a527c5c166fc90bc00727b387cd973a1bc96dbd
hzq <huangzq2030@gmail.com>
2016-08-02 17:58:51 +0800

    远程 database

--- config.js
+++ config.js
@@ -17,21 +17,21 @@ var config = {
 
     topn: 90,
 
-    db: {
+   /* db: {
         host: 'localhost',
         user: 'root',
         password: 'root',
         database: 'douyu',
         port: 3306
-    }
+    }*/
 
-    // db:{
-    //     host: 'localhost',
-    //     user: 'root',
-    //     password: 'xidian@513',
-    //     database: 'douyu',
-    //     port: 3306
-    // }
+    db:{
+        host: 'localhost',
+        user: 'root',
+        password: 'xidian@513',
+        database: 'douyu',
+        port: 3306
+    }
 }
 
 module.exports = config;
